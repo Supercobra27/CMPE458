40c11bbe21cb743da9edfe97a6f2cc08a8423636
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2025-02-03 12:58:03 -0500

    removed unecessary parentheses.

--- my-mini-compiler/phase1-w25/src/operators.c
+++ my-mini-compiler/phase1-w25/src/operators.c
@@ -39,12 +39,8 @@ static const char *const multi_operators[] = {
 int isOperatorStr(const char *_Str)
 {
     for (int i = 0; i < num_multi_operators; i++)
-    {
         if (strncmp(_Str, multi_operators[i], 2) == 0)
-        {
             return 2;
-        }
-    }
 
     if (isOperator(_Str[0]))
         return 1; // for single operators
