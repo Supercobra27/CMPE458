6e7c2ab746e162ee570f502e707c665164dfc19e
Supercobra27 <silverbergryan@gmail.com>
2025-03-06 17:15:01 -0500

    replaced for dynamic

--- my-mini-compiler/phase2-w25/src/lexer/lexer.c
+++ my-mini-compiler/phase2-w25/src/lexer/lexer.c
@@ -425,7 +425,7 @@ int main(int argc, char *argv[])
     int position = 0;
     Token token;
     init_lexer(&position, &current_line, &line_start);
-    TokenNode* head = init_tl();
+    Array *tokens = array_new(1, sizeof(Token));
     printf("Analyzing input:\n%s\n\n", input);
 
     // Get all tokens from input and print them.
@@ -433,11 +433,15 @@ int main(int argc, char *argv[])
     {
         token = get_next_token(input, &position);
         print_token(token);
-        insertEnd(&head, token);
+        array_push(tokens, &token);
     } while (token.type != TOKEN_EOF);
 
-    display(head);
-    free_tl_list(&head);
+    for (size_t i = 0; i < array_size(tokens); i++)
+    {
+        printf("Token # %zu - %s\n", i, ((Token*)array_get(tokens, i))->lexeme);
+    }
+
+    array_free(tokens);
 
     // Print line start positions.
     for (size_t i = 0; i < array_size(line_start); i++)
