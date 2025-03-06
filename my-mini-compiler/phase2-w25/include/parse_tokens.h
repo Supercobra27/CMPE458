7f4b122ad9a17cb15dc860dedbd43c0da358847a
Supercobra27 <silverbergryan@gmail.com>
2025-03-06 12:50:46 -0500

    reordered tokens for lexer

--- my-mini-compiler/phase2-w25/include/parse_tokens.h
+++ my-mini-compiler/phase2-w25/include/parse_tokens.h
@@ -4,8 +4,11 @@
 #include <stddef.h>
 
 typedef enum _ParseToken {
-    // Terminal tokens (these enum values must directly correspond to the token values in tokens.h)
+    PT_EOF,
     PT_IDENTIFIER,
+    PT_ERROR,
+
+    // Terminal tokens (these enum values must directly correspond to the token values in tokens.h)
     PT_INTEGER_CONST,
     PT_FLOAT_CONST,
     PT_STRING_CONST,
@@ -13,11 +16,6 @@ typedef enum _ParseToken {
     PT_INT_KEYWORD,
     PT_FLOAT_KEYWORD,
     PT_STRING_KEYWORD,
-
-    // Terminal nodes that don't appear in the AST.
-    PT_SEMICOLON,
-    PT_LEFT_BRACE,
-    PT_RIGHT_BRACE,
     PT_PRINT_KEYWORD,
     PT_IF_KEYWORD,
     PT_THEN_KEYWORD,
@@ -26,6 +24,11 @@ typedef enum _ParseToken {
     PT_REPEAT_KEYWORD,
     PT_UNTIL_KEYWORD,
     PT_FACTORIAL_KEYWORD,
+
+    // Terminal nodes that don't appear in the AST.
+    PT_SEMICOLON,
+    PT_LEFT_BRACE,
+    PT_RIGHT_BRACE,
     PT_LEFT_PAREN,
     PT_RIGHT_PAREN,
     PT_EOF,
