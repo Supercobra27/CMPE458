626e6cd67c773bee9e74885f4b01532259a1f584
Supercobra27 <silverbergryan@gmail.com>
2025-03-09 16:03:54 -0400

    started

---
+++ my-mini-compiler/phase3-w25/include/symbol_table.h
@@ -0,0 +1,11 @@
+#ifndef SYMTBL_H
+#define SYMTBL_H
+
+#include "dynamic_array.h"
+
+/**
+ * Symbol table header file for definition using the dynamic array structure and maybe a hash table.
+ * Looking into it
+*/
+
+#endif SYMTBL_H
